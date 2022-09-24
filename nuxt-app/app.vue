895dbdafa0a916ca8b2e60f1eecf5a21ee067ede
Andre-Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2022-09-24 09:29:41 +0200

    v1

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -1,5 +1,35 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <button class="button" @click="signIn">Sign In</button>
+    <button class="button" @click="signOut">Sign Out</button>
   </div>
 </template>
+
+<script setup>
+console.log("---APP---");
+
+const signIn = async () => {
+  const email = "sam@razorex.com";
+  const password = "123456";
+  const credentials = await signInUser(email,password);
+  console.log("Credentials",credentials);
+}
+
+const signOut = async () => {
+  const result = await signOutUser();
+  console.log("result", result);
+}
+
+onMounted(async () => {
+  //const email = "sam@razorex.com";
+  //const password = "123456";
+  //const credentials = await createUser(email,password);
+  //console.log("Credentials",credentials);
+
+});
+
+</script>
+
+<style>
+  @import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+</style>
