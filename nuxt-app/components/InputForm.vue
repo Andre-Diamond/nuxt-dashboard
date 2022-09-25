70407680dd2244a021ce5f00644b451fd2efcec8
Andre-Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2022-09-25 13:34:43 +0200

    Firebase store

---
+++ nuxt-app/components/InputForm.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+      <h2 class="mb-5 title is-4 has-text-black">{{ title }}</h2>
+      <div class="field">
+        <label class="label">What changed</label>
+        <div class="control has-icons-left has-icons-right">
+          <input
+            v-model="form.whatChanged"
+            class="input"
+            type="text"
+            placeholder="What changed"
+            autocomplete="false"
+          />
+          <span class="icon is-small is-left">
+            <i class="fas fa-cog"></i>
+          </span>
+        </div>
+      </div>
+      <div class="field">
+        <div>
+          <label class="label">Who initiated</label>
+          <div class="control has-icons-left has-icons-right">
+            <input
+              v-model="form.initiatedBy"
+              class="input"
+              type="text"
+              placeholder="IOG"
+              autocomplete="false"
+            />
+            <span class="icon is-small is-left">
+            <i class="fas fa-group"></i>
+            </span>
+          </div>
+        </div>
+      </div>
+  
+      <div v-if="message" class="has-text-success p-3">
+        {{ message }}
+      </div>
+  
+      <button class="button is-success" @click="$emit('submit')">Submit</button>
+    </div>
+  </template>
+  
+  <script setup>
+  const props = defineProps({
+    form: Object,
+    title: String,
+    message: String
+  });
+  </script>
+  
+  <style scoped></style>
--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -3,28 +3,51 @@
     <p><NuxtLink to="/secret">Go to Secret Page</NuxtLink></p>
     <button class="button" @click="signOut" v-if="firebaseUser">Sign out</button>
     <div v-if="firebaseUser">
+      <div class="container p-6">
+      <div class="column is-half is-offset-one-quarter">
+        <InputForm
+          class="has-background-info-dark box px-5 py-5 mx-4"
+          title="Enter Parameter change"
+          @submit="addData"
+          :form="registerForm"
+          :message="registerMessage"
+        />
+      </div>
+      </div>
       <client-only>
-        <pre
-          >user: {{ firebaseUser.email }}
+        <pre>
+          user: {{ firebaseUser.email }}
           {{ pages }}
-    </pre
-        >
+          <button class="button" @click="showData" v-if="firebaseUser">Show data</button>
+        </pre>
       </client-only>
     </div>
     <div v-else>User is signed out</div>
   </div>
 </template>
 
-<script setup>
+<script setup> 
 const firebaseUser = useFirebaseUser();
+const showRegisterForm = ref(true);
+const registerMessage = ref();
+const registerForm = ref({ whatChanged: "", initiatedBy: "" });
+const pages = ref();
 const signOut = async () => {
   await signOutUser();
 };
-const pages = ref();
-onMounted(async () => {
+const addData = async () => {
+  // Add a new document in collection "cities"
+   await $fetch("/api/add?col=pages", {
+      method: "POST",
+      body: {name: registerForm.value.whatChanged,
+      title: registerForm.value.initiatedBy,
+      description: "This is a test"}
+    })
+};
+const showData = async () => {
   const { result } = await $fetch("/api/query?col=pages");
   pages.value = result;
-});
+};
 </script>
 
 <style>
